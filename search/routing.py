70b5af768dbc7f6eb8cca38461519d1a3b829416
Grapsinho <92812712+Grapsinho@users.noreply.github.com>
2024-10-29 10:50:57 -0700

    Update routing.py

--- search/routing.py
+++ search/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import SearchConsumer
 
 websocket_urlpatterns = [
-    path('ws/search_friends/', SearchConsumer.as_asgi()),
+    path('wss/search_friends/', SearchConsumer.as_asgi()),
 ]
